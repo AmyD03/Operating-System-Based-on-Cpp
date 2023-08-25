7839bc96f85008ab86ad092d278b861829be0db7
Amy <dingxiaoyu168@outlook.com>
2023-08-25 16:59:57 +0800

    new desktop.h

---
+++ include/gui/desktop.h
@@ -0,0 +1,22 @@
+#ifndef __MYOS__GUI__DESKTOP_H
+#define __MYOS__GUI__DESKTOP_H
+
+#include <gui/widget.h>
+#include <drivers/mouse.h>
+
+
+namespace myos
+{
+    namspace gui
+    {
+        class Desktop : public CompositeWidget,public myos::drivers::MouseEventHandler
+        {
+            protected:
+                
+        };
+    }
+}
+
+
+
+#endif
